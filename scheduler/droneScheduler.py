a584f30c0f32db86223578ed1f7d437f1667dd84
Kaushal@BMS <31007568+Kaushalkushu@users.noreply.github.com>
2020-03-12 08:49:15 +0530

    Update droneScheduler.py

--- scheduler/droneScheduler.py
+++ scheduler/droneScheduler.py
@@ -6,67 +6,62 @@ import math
 
 class droneScheduler():
 
-    dronelist = []
-    itemlist = []
+    drones = deque()
+    items = deque()
+    destinations = deque() 
     maplist= []
-    destlist = []
     droneToItem= []
     itemToDestination= []
-    
+
     def distance(self,x1 , y1 , x2 , y2): 
         return math.hypot(x2 - x1, y2 - y1)
 
   
     def getdronelist(self):
         dq=droneQueue()
-# ENTER DRONES COORDINATES HERE
-
+    
+    # ENTER DRONES COORDINATES HERE
         dq.insert((4,9)) 
         dq.insert((7,2))
 
-        drones=dq.getQueue()
-        for i in range(0,len(drones)):
-            self.dronelist.append(drones[i])
+        self.drones = dq.getQueue()
     
     def getitemlist(self):
         iq = itemQueue()
 
-# ENTER ITEM COODRINATES HERE
+    # ENTER ITEM COODRINATES HERE
         iq.insert((2,5)) 
         iq.insert((10,5)) 
         iq.insert((10,3))
         iq.insert((6,3)) 
-        items = iq.getQueue()
-        for i in range(0, len(items)):
-            self.itemlist.append(items[i])
+        self.items = iq.getQueue()
+
 
     def getdestinationlist(self):
         desq = itemQueue()
 
-# ENTER ITEM DESTINATION COODRINATES HERE
+    # ENTER ITEM DESTINATION COODRINATES HERE
         desq.insert((10,10)) 
         desq.insert((1,1)) 
         desq.insert((3,8))
         desq.insert((8,3)) 
-        items = desq.getQueue()
-        for i in range(0, len(items)):
-            self.destlist.append(items[i])
-    
+        self.destinations = desq.getQueue()
+
     def createList(self,no):
 
         if no == 1:
-            for i in self.dronelist:
-                self.droneToItem.append(self.distance(i[0],i[1],self.itemlist[0][0],self.itemlist[0][1]))
+            for i in range(len(self.drones)):
+                self.droneToItem.append(self.distance(self.drones[i][0],self.drones[i][1],self.items[0][0],self.items[0][1]))
 
         elif no == 2:
-            for i in range(len(self.itemlist)):
-                self.itemToDestination.append(self.distance(self.itemlist[i][0],self.itemlist[i][1],self.destlist[i][0],self.destlist[i][1]))
+            for i in range(len(self.items)):
+                self.itemToDestination.append(self.distance(self.items[i][0],self.items[i][1],self.destinations[i][0],self.destinations[i][1]))
 
     
 
     def schedule(self):
 
-        try:
+        # try:
 
             self.getdronelist()
             self.getitemlist()
@@ -78,30 +73,30 @@ class droneScheduler():
             for i in range(len(self.droneToItem)):
                 li.append( self.droneToItem[i] + self.itemToDestination[0] )    
 
-            while len(self.itemlist)!=0:
+            while len(self.items)!=0:
                     print('droneToItem',self.droneToItem)
                     print('itemToDestination',self.itemToDestination)
                     print('li',li)     
 
                     droneNo = li.index(min(li))
-                    print('Drone',droneNo,' =>Coordinates ',self.itemlist[0] )
+                    print('Drone',droneNo,' =>Coordinates ',self.items[0] )
                     
                     li.clear()
                     self.droneToItem.clear()
                     self.itemToDestination.clear()
                     self.maplist.append(droneNo)
-                    del self.itemlist[0]
-                    pos = self.destlist.pop(0)
-                    self.dronelist[droneNo] = pos
-                    if len(self.itemlist) != 0:
+                    del self.items[0]
+                    pos = self.destinations.popleft()
+                    self.drones[droneNo] = pos
+                    if len(self.items) != 0:
                         self.createList(1)
                         self.createList(2)
                         for i in range(len(self.droneToItem)):
                             li.append( self.droneToItem[i] + self.itemToDestination[0] )
                     print('==============')
         
-        except:
-            print('Check if both item and destination position is available')
+        # except:
+        #     print('Check if both item and destination position is available')
 
 
 obj = droneScheduler()
