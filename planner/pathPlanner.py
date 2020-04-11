7a6ca0bee401680b53b2e52da667509fe0723019
Kaushal@BMS <31007568+Kaushalkushu@users.noreply.github.com>
2020-04-11 21:32:57 +0530

    Add files via upload

---
+++ planner/pathPlanner.py
@@ -0,0 +1,95 @@
+# from tello_commands import TelloClass
+
+
+
+class pathPanner:
+
+    def __init__(self):
+
+        self.direction = 0 
+        '''
+        0-> North
+        1-> East
+        2->South
+        3->West
+        '''
+    def start(self):
+        # TelloClass.create_tello_object(self)
+        # TelloClass.tello_connect(self)
+        # TelloClass.tello_takeoff(self)
+        print('Took off')
+
+    def end(self):
+        # TelloClass.tello_land(self)
+        # TelloClass.tello_end(self)
+        print('landed')
+
+    def updateDirection(self, turn):
+        if turn=='r':
+            print('Turned Right')
+            self.direction = (self.direction+1)%4
+        elif turn=='l':
+            print('Turned Left')
+            self.direction = (self.direction+3)%4
+
+    def firstQuadrant(self, resx=20, resy=20):
+        self.start()
+        while(self.direction!=0):
+            self.updateDirection('r')
+        # TelloClass.tello_forward_cm(self,resy)
+        print('Forward'+ str(resy))
+        # TelloClass.tello_right_cm(self,resx)
+        print('Right'+ str(resx))        
+        self.end()
+
+    def secondQuadrant(self, resx=20, resy=20):
+        self.start()
+        while(self.direction!=0):
+            self.updateDirection('r')
+        # TelloClass.tello_forward_cm(self,resy)
+        print('Forward'+ str(resy))
+        # TelloClass.tello_left_cm(self,resx)  
+        print('Left'+ str(resx))      
+        self.end()
+        
+    def thirdQuadrant(self, resx=20, resy=20):
+        self.start()
+        while(self.direction!=2):
+            self.updateDirection('r')
+        # TelloClass.tello_forward_cm(self,resy)
+        print('Forward'+ str(resy))
+        # TelloClass.tello_right_cm(self,resx) 
+        print('Right'+ str(resx))       
+        self.end()
+
+    def fourthQuadrant(self, resx=20, resy=20):
+        self.start()
+        while(self.direction!=2):
+            self.updateDirection('r')
+        # TelloClass.tello_forward_cm(self,resy)
+        print('Forward'+ str(resy))
+        # TelloClass.tello_left_cm(self,resx)  
+        print('Left'+ str(resx))      
+        self.end()
+
+
+    def plan(self, src, dst):
+        x1, y1 = src[0], src[1]
+        x2, y2 = dst[0], dst[1]
+        resx, resy = x2-x1, y2-y1
+        print(resx, resy)
+        if resx>0 and resy>0:
+            self.firstQuadrant(resx, resy)
+        elif resx<0 and resy>0:
+            self.secondQuadrant(resx*-1, resy)
+        elif resx<0 and resy<0:
+            self.thirdQuadrant(resx*-1, resy*-1)
+        elif resx>0 and resy<0:
+            self.fourthQuadrant(resx, resy*-1)
+
+
+obj = pathPanner()
+src = tuple(map(int,input('Enter src coordinates: ').split()))
+dst = tuple(map(int,input('Enter dst coordinates: ').split()))
+print(src, dst)
+obj.plan(src, dst)
---
+++ planner/tello_commands.py
@@ -0,0 +1,43 @@
+from TelloSDKPy.djitellopy import Tello
+
+
+class TelloClass:
+
+    def create_tello_object(self):
+        self.tello = Tello()
+
+    def tello_connect(self):
+        self.tello.connect()
+
+    def tello_takeoff(self):
+        self.tello.takeoff()
+
+    def tello_left_cm(self, distance):
+        self.tello.move_left(distance)
+
+    def tello_right_cm(self, distance):
+        self.tello.move_right(distance)
+
+    def tello_up_cm(self, distance):
+        self.tello.move_up(distance)
+
+    def tello_down_cm(self, distance):
+        self.tello.move_down(distance)
+
+    def tello_back_cm(self, distance):
+        self.tello.move_back(distance)
+
+    def tello_forward_cm(self, distance):
+        self.tello.move_forward(distance)
+
+    def tello_rotate_counter_clockwise(self, angle):
+        self.tello.rotate_counter_clockwise(angle)
+
+    def tello_rotate_clockwise(self, angle):
+        self.tello.rotate_clockwise(angle)
+
+    def tello_land(self):
+        self.tello.land()
+
+    def tello_end(self):
+        self.tello.end()
